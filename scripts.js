5e925f1f4f330a1090b48516dee7eb681c76c911
Abdallah al khassaky <66669029+debugleader@users.noreply.github.com>
2020-08-27 08:25:45 +0400

    Added html,css,js and images

---
+++ scripts.js
@@ -0,0 +1,8 @@
+//start
+const selectElement = (s) => document.querySelector(s);
+selectElement('.open').addEventListener('click', () => {
+    selectElement('.nav-list').classList.add('active');
+});
+selectElement('.close').addEventListener('click', () => {
+    selectElement('.nav-list').classList.remove('active');
+});
